b08dee1d94cd15046e36901d70a382502bf71977
KKamjangE <ajm980718@gmail.com>
2022-08-12 13:12:25 +0900

    Momentum-App

--- js/greetings.js
+++ js/greetings.js
@@ -14,7 +14,7 @@ function onLoginSubmit(e) {
 }
 
 function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`
+    greeting.innerText = `Hello ${username}!!`
     greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
--- js/todo.js
+++ js/todo.js
@@ -23,7 +23,7 @@ function paintToDo(newTodo) { //li 생성
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "❌";
+    button.innerText = "Delete?";
     button.addEventListener('click', deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
